9f2b0b54f1a68a8a79694ae5818c61cfb5f8d3d3
David Muto <david.muto@gmail.com>
2013-08-08 10:20:48 -0400

    expand tabs on save

--- ExpandTabsOnSave/ExpandTabsOnSave.py
+++ ExpandTabsOnSave/ExpandTabsOnSave.py
@@ -1,6 +1,7 @@
 import sublime, sublime_plugin, os
 
 class ExpandTabsOnSave(sublime_plugin.EventListener):
+	# expand tabs to spaces
 	def on_pre_save(self, view):
 		if view.settings().get('expand_tabs_on_save') == 1:
 			view.window().run_command('expand_tabs')
